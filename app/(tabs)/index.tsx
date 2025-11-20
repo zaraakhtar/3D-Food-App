9864c86fe427df0169b90a245279e307b4eb7a86
zaraakhtar <zaramalikawan9@gmail.com>
2025-11-20 12:03:38 +0500

    added tailwind css

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -5,7 +5,7 @@ import { SafeAreaView } from 'react-native-safe-area-context';
 export default function HomeScreen() {
   return (
     <SafeAreaView>
-          <View>
+          <View className=''>
             <Text>
               Hello, World!
             </Text>
