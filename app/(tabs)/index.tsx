6f0ab2ae31c523ec9439055215f68c7011cb0ad2
zaraakhtar <zaramalikawan9@gmail.com>
2025-11-20 18:00:08 +0500

    tailwind correct version probelm solved

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,12 +1,13 @@
 import { Text, View } from 'react-native';
 import 'react-native-gesture-handler';
 import { SafeAreaView } from 'react-native-safe-area-context';
+import './../../global.css';
 
 
 export default function HomeScreen() {
   return (
     <SafeAreaView>
-          <View className='justify-center items-center text-xl font-bold h-full'>
+          <View className='flex-1 justify-center items-center'>
             <Text>
               Hello, World!
             </Text>
